bb742344c03a3e9356c2239f6a83ae055451b5fd
Gabor Hozleiter <hozleitergabor@gmail.com>
2024-09-25 23:45:34 +0200

    added dashboard-header

---
+++ src/app/main/dashboard/dashboard-header/dashboard-header.component.ts
@@ -0,0 +1,39 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { CoinService } from '../../../coin-service.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard-header.component.html',
+  styleUrl: './dashboard-header.component.scss'
+})
+export class DashboardHeaderComponent {
+
+  coins: any[] = [];
+
+  constructor(private coinService: CoinService) {}
+
+  ngOnInit(): void {
+    this.coinService.getCoins().subscribe((data) => {
+      this.coins = data.data;
+    });
+  }
+
+  @ViewChild('coinContainer') coinContainer!: ElementRef;
+
+  scrollLeft() {
+    this.coinContainer.nativeElement.scrollBy({
+      left: -150, // Scroll nach links (z.B. 150px)
+      behavior: 'smooth'
+    });
+  }
+
+  scrollRight() {
+    this.coinContainer.nativeElement.scrollBy({
+      left: 150, // Scroll nach rechts (z.B. 150px)
+      behavior: 'smooth'
+    });
+  }
+}
